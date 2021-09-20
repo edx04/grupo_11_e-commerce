8db8eb7f072b721e1335ac009a25e765acb86285
Edgar Arellano <ed04alb@gmail.com>
2021-09-20 15:12:10 -0500

    structure database

---
+++ structure.sql
@@ -0,0 +1,87 @@
+DROP DATABASE IF EXISTS music_db;
+
+CREATE DATABASE music_db;
+USE music_db;
+
+DROP TABLE IF EXISTS `brands`;
+CREATE TABLE `brands` (
+	`id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) NOT NULL,
+    PRIMARY KEY (`id`)
+);
+
+DROP TABLE IF EXISTS `categories`;
+CREATE TABLE `categories` (
+	`id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) NOT NULL,
+PRIMARY KEY (`id`)
+);
+
+DROP TABLE IF EXISTS `colors`;
+CREATE TABLE `colors` (
+	`id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) NOT NULL,
+PRIMARY KEY (`id`)
+);
+
+
+DROP TABLE IF EXISTS `products`;
+CREATE TABLE `products` (
+	`id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) NOT NULL,
+  `price` int(11) NOT NULL,
+  `discount` int(11) NOT NULL DEFAULT 0,
+  `description` varchar(200) NOT NULL,
+  `image` varchar(100) NOT NULL DEFAULT 'default.jpg',
+  `id_color` int(11) ,
+  `id_categories` int(11) ,
+  `id_brand` int(11) , 
+   PRIMARY KEY (`id`),
+   KEY `products_brand_id_foreign` (`id_brand`),
+   KEY `products_categories_id_foreign` (`id_categories`),
+   KEY `products_color_id_foreign` (`id_color`),
+   CONSTRAINT `products_brand_id_foreign` FOREIGN KEY (`id_brand`) REFERENCES `brands` (`id`),
+   CONSTRAINT `products_categories_id_foreign` FOREIGN KEY (`id_categories`) REFERENCES `categories` (`id`),
+   CONSTRAINT `products_color_id_foreign` FOREIGN KEY (`id_color`) REFERENCES `colors` (`id`)
+);
+
+
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE `users`(
+   `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `password`varchar(50) NOT NULL ,
+  `image` varchar(100) NOT NULL DEFAULT 'default.jpg',
+  PRIMARY KEY (`id`)
+);
+
+DROP TABLE IF EXISTS `orders`;
+CREATE TABLE `orders`(
+   `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id_user` int(11) NOT NULL,
+  `total` int(11) NOT NULL,
+  `date` datetime, 
+   PRIMARY KEY (`id`),
+   KEY `orders_user_id_foreign` (`id_user`),
+   CONSTRAINT `orders_user_id_foreignn` FOREIGN KEY (`id_user`) REFERENCES `users` (`id`)
+);
+
+DROP TABLE IF EXISTS `orderProducts`;
+CREATE TABLE `orderProducts`(
+	`id` int(11) NOT NULL AUTO_INCREMENT,
+  `id_order` int(11) NOT NULL,
+  `id_product` int(11) NOT NULL,
+  `quantity` int(11) NOT NULL,
+  `subtotal` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `orderproduct_order_id_foreign` (`id_order`),
+  KEY `orderproduct_product_id_foreign` (`id_product`),
+  CONSTRAINT `orderproduct_order_id_foreign` FOREIGN KEY (`id_order`) REFERENCES `orders` (`id`),
+  CONSTRAINT `orderproduct_product_id_foreign` FOREIGN KEY (`id_product`) REFERENCES `products` (`id`)
+);
+
+
+
+
+
