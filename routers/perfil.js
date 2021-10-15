87440c2f6e3376a57d05fa2d34a43e5afd85a33d
Miguel Mora <miguelmora6095@gmail.com>
2021-10-15 15:59:00 -0500

    Funcion de actualizar perfil

--- routers/perfil.js
+++ routers/perfil.js
@@ -1,14 +1,31 @@
 const express = require('express');
 const router = express.Router();
+const multer = require("multer");
+const path = require("path");
 
 const perfilController = require('../controllers/perfilController');
 
 const invitadosMiddleware = require("../middlewares/invitadosMiddleware");
 
+
+//Configuración para multer
+let storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, "public/images/users")
+    },
+    filename: function (req, file, cb) {
+      cb(null, file.fieldname + "-" + Date.now() + path.extname(file.originalname))
+    }
+  })
+  
+let upload = multer({ storage: storage })
+
 router.get('/', invitadosMiddleware ,perfilController.index);
 
 router.get('/editar', invitadosMiddleware ,perfilController.edit);
 
-router.post('/editar', invitadosMiddleware ,perfilController.update);
+router.post('/editar', upload.single("image"), invitadosMiddleware ,perfilController.update);
+
+router.get('/pedidos', invitadosMiddleware ,perfilController.orders);
 
 module.exports = router;
